0bb759da2de5cfeca9ef4e19a907441d71c19018
yuvacn04-oss <yuvacn04@gmail.com>
2026-08-29 08:11:21 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -26,10 +26,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor #(1,2) (ps,  a,   b);
-  and #(1,2) (pc1, a,   b);
-  xor #(2,3) (sum, cin, ps);
-  and #(1,2) (pc2, cin, ps);
-  or  #(2,1) (cout, pc1, pc2);
+  xor #(2) (ps,  a,   b);
+  and #(2) (pc1, a,   b);
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+  or  #(2) (cout, pc1, pc2);
 
 endmodule
